2aba469dfe699fdf1757ada9bad7d2c334fe733b
Tomer Zaidentein <ztomer@gmail.com>
2026-06-17 09:30:42 -0400

    fork: remove commercial/cloud + cactus (upstream paths the fork drops)

--- apps/web/src/routes/_view/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_view/app/")({
-  beforeLoad: async () => {
-    throw redirect({
-      to: "/app/account/",
-    });
-  },
-});
